f916c10031983658943922f2c624cce580b06a11
CRThu <1723001388@qq.com>
2018-01-07 15:21:05 +0800

    update VariableType

---
+++ VariableType/VariableType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt;
+            string myStr,myAtStr,myFileStr;
+            myInt = 123456;
+            myStr = "\"myInt\" is";
+            myAtStr = @"This string has a
+line break.";
+            myFileStr = @"C:\Users";
+            Console.WriteLine($"{myStr} {myInt}");
+            Console.WriteLine("This string has a\nline break.");
+            Console.WriteLine(myAtStr);
+            Console.WriteLine(myFileStr);
+            Console.ReadKey();
+        }
+    }
+}
