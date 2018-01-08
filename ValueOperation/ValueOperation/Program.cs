356b6069b566552193facfac52ffadbd7bed3c04
CRThu <1723001388@qq.com>
2018-01-08 20:10:19 +0800

    update ValueOperation

---
+++ ValueOperation/ValueOperation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first, next;
+            string userName;
+            Console.WriteLine("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine($"welcome {userName}!");
+            Console.WriteLine("Enter first number:");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter next number:");
+            next = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The + of {first}&{next}" + "=" + $"{first + next}.");
+            Console.WriteLine($"The - of {first}&{next}" + "=" + $"{first - next}.");
+            Console.WriteLine($"The * of {first}&{next}" + "=" + $"{first * next}.");
+            Console.WriteLine($"The / of {first}&{next}" + "=" + $"{first / next}.");
+            Console.WriteLine($"The % of {first}&{next}" + "=" + $"{first % next}.");
+            Console.ReadKey();
+        }
+    }
+}
