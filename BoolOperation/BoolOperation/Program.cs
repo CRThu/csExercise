d832f7c7fd272e60421cd02cf2209db0c20d85dc
CRThu <1723001388@qq.com>
2018-01-09 19:17:46 +0800

    update BoolOperation

---
+++ BoolOperation/BoolOperation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using static ... 允许使用静态类的方法
+using static System.Console;
+using static System.Convert;
+
+namespace BoolOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter an integer:");
+            int myInt = ToInt32(ReadLine());
+            bool isLessThan10 = myInt < 10;
+            bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+            WriteLine($"isLessThan10:{isLessThan10}");
+            WriteLine($"isBetween0And5:{isBetween0And5}");
+            WriteLine($"is exactly one of above is true:{isLessThan10 ^ isBetween0And5}");
+            ReadKey();
+        }
+    }
+}
