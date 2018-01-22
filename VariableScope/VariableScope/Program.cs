bb854fa1085d4b2085a102f812fb6a974a86f6ec
CRThu <1723001388@qq.com>
2018-01-22 22:22:35 +0800

    update VariableScope

---
+++ VariableScope/VariableScope/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace VariableScope
+{
+    class Program
+    {
+        static string myString;
+        static void Write()
+        {
+            string myString = "String defined in Write()";
+            WriteLine("Now in Write()");
+            WriteLine($"Local myString = {myString}");
+            WriteLine($"Global myString = {Program.myString}");
+        }
+
+        static void Main(string[] args)
+        {
+            string myString = "String defined in Main()";
+            Program.myString = "Global string";
+            Write();
+            WriteLine("\nNow in Main()");
+            WriteLine($"Local myString = {myString}");
+            WriteLine($"Global myString = {Program.myString}");
+
+            WriteLine("\nTest in Loop:");
+            int i = 0;
+            string text = "";
+            for (i = 0; i < 10; i++)
+            {
+                text = "Line " + Convert.ToString(i);
+                WriteLine($"{text}");
+            }
+            WriteLine($"Last text output in loop: {text}");
+            ReadKey();
+        }
+    }
+}
