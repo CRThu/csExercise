20bf2178f44ccc0cb581fb2b36fd96f3e2f1fd0a
CRThu <1723001388@qq.com>
2018-01-24 18:40:23 +0800

    update structFunction

---
+++ structFunction/structFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace structFunction
+{
+    struct CustomerName
+    {
+        public string firstName, lastName;
+        public string Name() => firstName + " " + lastName;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerName myCustomer;
+            myCustomer.firstName = "John";
+            myCustomer.lastName = "Franklin";
+            WriteLine($"{myCustomer.firstName} {myCustomer.lastName}");
+            WriteLine(myCustomer.Name());
+            ReadKey();
+        }
+    }
+}
