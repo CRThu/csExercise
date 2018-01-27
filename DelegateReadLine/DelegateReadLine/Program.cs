408a639ac3bba077158d33b09471af14e1d7871d
CRThu <1723001388@qq.com>
2018-01-27 22:07:38 +0800

    update DelegateReadLine

---
+++ DelegateReadLine/DelegateReadLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DelegateReadLine
+{
+    class Program
+    {
+        delegate string ReadLineDelegate();
+        static void Main(string[] args)
+        {
+            ReadLineDelegate readLine = new ReadLineDelegate(ReadLine);
+            WriteLine("Type a string:");
+            string userInput = readLine();
+            WriteLine($"You typed: {userInput}");
+            ReadKey();
+        }
+    }
+}
