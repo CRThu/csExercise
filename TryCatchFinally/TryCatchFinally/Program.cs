7ce80063053cb68b4a27a22b1bc29a8f874f0635
CRThu <1723001388@qq.com>
2018-01-29 21:48:16 +0800

    update TryCatchFinally

---
+++ TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 8;
+            try
+            {
+                j = j / i;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"i = {i}, j= {j}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
