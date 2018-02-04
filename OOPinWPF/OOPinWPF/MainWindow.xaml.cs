b35ad78d35dc9835d7c44ad19441d485ae17f48e
CRThu <1723001388@qq.com>
2018-02-04 23:21:44 +0800

    update OOPinWPF

--- OOPinWPF/OOPinWPF/MainWindow.xaml.cs
+++ OOPinWPF/OOPinWPF/MainWindow.xaml.cs
@@ -27,11 +27,19 @@ namespace OOPinWPF
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            ((Button)sender).Content = "Clicked!";
-            Button newButton = new Button();
+            // 监听Button类提供的Click事件
+            // sender 为 button
+            ((Button)sender).Content = "Clicked!";  // Object类强制转换为Button类 修改Content属性
+            Button newButton = new Button();    // new Button类实例
             newButton.Content = "New Button!";
-            newButton.Margin = new Thickness(10, 10, 200, 200);
-            // TODO
+            newButton.Margin = new Thickness(10, 10, 200, 200); //Margin属性使用Thickness对象配置
+            newButton.Click += newButton_Click; // 添加事件处理程序以响应newButton的Click事件
+            ((Grid)((Button)sender).Parent).Children.Add(newButton);    // 父对象转换为Grid类添加至窗口
+        }
+        private void newButton_Click(object sender,RoutedEventArgs e)
+        {
+            // 监听newButton类提供的Click事件
+            ((Button)sender).Content = "Clicked!";
         }
     }
 }
