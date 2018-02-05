85a7473f83393c458779383edfb53ac8dd5d2efb
CRThu <1723001388@qq.com>
2018-02-05 22:53:40 +0800

    update ClassDefine

---
+++ ClassDefine/ClassDefine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDefine
+{
+    class Program
+    {
+        class MyClass
+        {
+            // Class members.
+        }
+        internal  class MyInternalClass
+        {
+            // Class members.
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
