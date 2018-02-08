45a8dc8107b342577c621f7382d5c913e9fe6bc8
CRThu <1723001388@qq.com>
2018-02-08 23:07:05 +0800

    update ClassDefine

--- ClassDefine/ClassDefine/Program.cs
+++ ClassDefine/ClassDefine/Program.cs
@@ -8,14 +8,48 @@ namespace ClassDefine
 {
     class Program
     {
+        // internal & public
         class MyClass
         {
             // Class members.
         }
-        internal  class MyInternalClass
+        internal class MyInternalClass
+        {
+            // Class members.
+        }   // 不加修饰符相当于internal 只有当前项目中的代码才能访问
+        public class MyPublicClass
+        {
+            // Class members.
+        }   // 可由其他项目中的代码访问
+
+        // abstract & sealed
+        public abstract class MyPublicAbstractClass
+        {
+            // Class members, may be abstract.
+        }   // 抽象的 不能实例化,只能继承,可以有抽象成员
+        public sealed class MyPublicSealedClass
+        {
+            // Class members.
+        }   // 密封的 只能实例化,不能继承
+
+        // 继承
+        public class MyBigClass : MyPublicClass, IMyInterface
         {
             // Class members.
         }
+
+        // 接口
+        // 不能使用 abstract 和 sealed 关键字,接口定义不包含实现代码,不能直接实例化,且必须是可以继承的
+        interface IMyInterface
+        {
+            // Interface members.
+        }   // 默认internal
+        interface IMyPublicInterface
+        {
+            // Interface members.
+        }   // 可以公开访问
+
+
         static void Main(string[] args)
         {
         }
