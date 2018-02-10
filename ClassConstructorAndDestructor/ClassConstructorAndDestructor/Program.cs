628c6aaa172a4f9ced2e76f53db353ff7eea1d0a
CRThu <1723001388@qq.com>
2018-02-10 23:55:56 +0800

    update ClassConstructorAndDestructor

---
+++ ClassConstructorAndDestructor/ClassConstructorAndDestructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConstructorAndDestructor
+{
+    class MyPublicClass
+    {
+        public MyPublicClass()
+        {
+            // Constructor code.
+        }   // 构造函数
+    }
+    class MyPrivateClass
+    {
+        private MyPrivateClass()
+        {
+            // Constructor code.
+        }   // 私有构造函数,不可创建
+    }
+
+    // TODO
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
