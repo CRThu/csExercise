344e850cf262ab46842d7ad5dde87de7cfda8455
CRThu <1723001388@qq.com>
2018-02-14 23:48:08 +0800

    update summary

---
+++ summary/summary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summary
+{
+    class Program
+    {
+        // 视图 - 对象浏览器 查看
+
+        /// <summary>
+        /// 这里是主函数说明
+        /// </summary>
+        /// <param name="args">这里是args说明</param>
+        static void Main(string[] args)
+        {
+        }
+        /// <summary>
+        /// 这里是func函数说明
+        /// </summary>
+        /// <param name="a">这里是参数a说明</param>
+        /// <param name="b">这里是参数b说明</param>
+        /// <returns>这里是返回值说明</returns>
+        int func(int a,string b)
+        {
+            return 0;
+        }
+    }
+}
