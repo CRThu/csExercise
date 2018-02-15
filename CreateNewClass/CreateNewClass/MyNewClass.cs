f873954ab58a4fda23dc5f4d17e1b4815e379fc3
CRThu <1723001388@qq.com>
2018-02-15 23:29:52 +0800

    update CreateNewClass

---
+++ CreateNewClass/CreateNewClass/MyNewClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateNewClass
+{
+    // 项目 - 添加类 - 类 添加新类
+    class MyNewClass
+    {
+    }
+}
