174ff6b9896105cc7baaf2ef3f134f635a940b76
CRThu <1723001388@qq.com>
2018-02-20 23:56:13 +0800

    update ClassMember

---
+++ ClassMember/ClassMember/MyBaseClassAndMyDerivedClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMember
+{
+    // 不写new可以隐藏基类, 但会产生警告CS0108
+    class MyBaseClassNothing
+    {
+        public void DoSomething() => Console.WriteLine("MyBaseClassNothing.DoSomething()");
+    }
+    class MyDerivedClassNothing : MyBaseClassNothing
+    {
+        public void DoSomething() => Console.WriteLine("MyDerivedClassNothing.DoSomething()");  // CS0108
+    }
+    // 等同于使用new, 显式表明隐藏该成员
+    class MyDerivedClassNew : MyBaseClassNothing
+    {
+        new public void DoSomething() => Console.WriteLine("MyDerivedClassNew.DoSomething()");
+    }
+
+    // virtual和override用于重写
+    public class MyBaseClassVirtual
+    {
+        public virtual void DoSomething() => Console.WriteLine("MyBaseClassVirtual.DoSomething()");
+    }
+    public class MyDerivedClassOverride : MyBaseClassVirtual
+    {
+        public override void DoSomething() => Console.WriteLine("MyDerivedClassOverride.DoSomething()");
+    }
+}
--- ClassMember/ClassMember/Program.cs
+++ ClassMember/ClassMember/Program.cs
@@ -6,86 +6,14 @@ using System.Threading.Tasks;
 
 namespace ClassMember
 {
-    class MyClass
-    {
-        // 成员定义 :
-        // static : 静态成员, class level, 所有实例共享
-        // 修饰符 :
-        // public : 成员可以由任何代码访问
-        // private : 成员只能由类中的代码访问 (默认)
-        // internal : 成员只能由定义它的程序集/项目的内部代码访问
-        // protected : 成员只能由类或派生类的代码访问
-        // 定义字段
-        // const : 静态常量, 在声明时被初始化
-        // readonly : 动态常量, 只能在执行构造函数的过程中赋值或初始化赋值语句赋值
-        public int MyInt;
-        public readonly int MyReadonlyInt = 17;
-        public static int MyStaticInt;
-        // 定义方法
-        // virtual : 方法可以重写
-        // abstract : 方法必须在非抽象的派生类中重写 (只用于抽象类中)
-        // override : 方法重写了一个基类方法 (如果方法被重写, 就必须使用该关键字)
-        // extern : 方法定义放在其他地方
-        public string GetString() => "Here is a string, MyIntProp = " + MyIntProp;
-        public override string ToString() => "Override : " + base.ToString();
-        public virtual void DoString()
-        {
-            // Base implementation.
-        }
-        // 定义属性
-        // Field used by property.
-        private int MyPrivateInt;
-        // Property.
-        public int MyIntProp
-        {
-            // 访问器 get/set
-            get
-            {
-                // Property get code.
-                return MyPrivateInt;
-            }
-            // protected set
-            set
-            {
-                // Property set code.
-                if (value >= 0 && value <= 10)
-                    MyPrivateInt = value;   // value 等于类型与属性相同的一个值
-                else
-                    throw (new ArgumentOutOfRangeException("MyIntProp", value, "MyIntProp must be assigned a value between 0 and 10."));
-            }
-        }
-        // Field used by property.
-        private int myDoubledInt = 5;
-        // Property.
-        public int MyDoubledIntProp => (myDoubledInt * 2);
 
-        // 重构成员 refactoring
-        public string MyString  // 自动生成
-        {
-            get { return myString; }
-            set { myString = value; }
-        }
-        private string myString; // 右键 - 快速操作和重构
-
-        // 自动属性
-        // 键入prop后两次Tab
-        public int MyProperty { get; set; }
-        public int MyPropertyReadonly1 { get; }
-        public int MyPropertyReadonly2 { get; } = 9;
-        
-    }
-    class MyDerivedClass : MyClass
-    {
-        public override sealed void DoString()  // sealed 指定不能再有派生类重写
-        {
-            // Derived class implementation, overrides base implementation.
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             MyClass myObj = new MyClass();
+            Console.WriteLine($"Created myObj, Name = " + myObj.Name);
+            Console.WriteLine();
             for (int i = -1; i <= 11; i++)
             {
                 try
@@ -108,6 +36,14 @@ namespace ClassMember
             Console.Write("myObj.MyDoubledIntProp = ");
             Console.WriteLine(myObj.MyDoubledIntProp);
 
+            Console.WriteLine();
+            Console.WriteLine("隐藏和重写测试 :");
+            MyBaseClassNothing myBaseClassNothing1 = new MyDerivedClassNothing();    // 使用派生类构造函数构造基类
+            myBaseClassNothing1.DoSomething();  // 基类的函数被隐藏
+            MyBaseClassNothing myBaseClassNothing2 = new MyDerivedClassNew();   // 不加相当于new
+            myBaseClassNothing2.DoSomething();  // 基类的函数被隐藏
+            MyBaseClassVirtual myBaseClassVirtual = new MyDerivedClassOverride();   // 重写
+            myBaseClassVirtual.DoSomething();   // 基类的函数被重写
             Console.ReadKey();
         }
     }
