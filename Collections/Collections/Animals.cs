134df4896ef423370bdfbc9bd87f945813687865
CRThu <1723001388@qq.com>
2018-03-11 22:29:13 +0800

    update Collections

--- Collections/Collections/Animals.cs
+++ Collections/Collections/Animals.cs
@@ -9,11 +9,20 @@ namespace Collections
 {
     // Animals集合
     // CollectionBase类和indexer索引符
-    public class Animals:CollectionBase
+    public class Animals : CollectionBase
     {
         public void Add(Animal newAnimal)
         {
             List.Add(newAnimal);
+        }
+        public void Remove(Animal newAnimal)
+        {
+            List.Remove(newAnimal);
+        }
+        public Animal this[int animalIndex]
+        {
+            get { return (Animal)List[animalIndex]; }
+            set { List[animalIndex] = value; }
         }
     }
 }
