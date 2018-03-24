4fd7d67728eaa082b04241b0d6830b7b44d4c189
CRThu <1723001388@qq.com>
2018-03-24 23:28:41 +0800

    update compare

---
+++ compare/compare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compare
+{
+    class Program
+    {
+        struct MyStruct
+        {
+            public int Val;
+        }
+        static void Main(string[] args)
+        {
+            // 封箱&拆箱
+            MyStruct valType1 = new MyStruct();
+            valType1.Val = 5;
+            object refType = valType1;  // 封箱
+            valType1.Val = 6;
+            MyStruct valType2 = (MyStruct)refType;  // 拆箱
+            Console.WriteLine($"valType2.Val = {valType2.Val}");
+            
+            Console.ReadKey();
+        }
+    }
+}
