f45ab9aea1497d16bf60a75d479b0cc785a2c3e3
CRThu <1723001388@qq.com>
2018-03-29 22:26:09 +0800

    update CardClassLibrary

--- CardClassLibrary/CardClassLibrary/Card.cs
+++ CardClassLibrary/CardClassLibrary/Card.cs
@@ -29,6 +29,7 @@ namespace CardClassLibrary
         {
             return "The " + rank + " of " + suit + "s";
         }
+
         /// <summary>
         /// Flag for trump usage. If true, trumps are valued higher than cards of other suits.
         /// </summary>
@@ -41,5 +42,75 @@ namespace CardClassLibrary
         /// Flag that determines whether aces are higher than kings or lower than deuces.
         /// </summary>
         public static bool isAceHigh = true;
+
+        // 运算符重载
+        public static bool operator ==(Card card1, Card card2)
+            => (card1?.suit == card2?.suit) && (card1?.rank == card2?.rank);
+        public static bool operator !=(Card card1, Card card2)
+            => !(card1 == card2);
+        public override bool Equals(object card) => this == (Card)card;
+        public override int GetHashCode() => 13 * (int)suit + (int)rank;
+        public static bool operator >(Card card1, Card card2)
+        {
+            if (card1.suit == card2.suit)
+            {
+                if (isAceHigh)  // 检测使能Ace
+                {
+                    if (card1.rank == Rank.Ace) // 检测Ace
+                    {
+                        if (card2.rank == Rank.Ace) // 检测Ace
+                            return false;
+                        else
+                            return true;
+                    }
+                    else
+                    {
+                        if (card2.rank == Rank.Ace)
+                            return false;
+                        else
+                            return (card1.rank > card2?.rank);
+                    }
+                }
+                else
+                    return (card1.rank > card2.rank);   // 非使能Ace比较
+            }
+            else
+            {
+                if (useTrumps && (card2.suit == Card.trump))    // 检测王牌
+                    return false;
+                else
+                    return true;
+            }
+        }
+        public static bool operator <(Card card1, Card card2)
+            => !(card1 >= card2);
+        public static bool operator >=(Card card1, Card card2)
+        {
+            if (card1.suit == card2.suit)
+            {
+                if (isAceHigh)
+                {
+                    if (card1.rank == Rank.Ace)
+                        return true;
+                    else
+                    {
+                        if (card2.rank == Rank.Ace)
+                            return false;
+                        else
+                            return (card1.rank >= card2.rank);
+                    }
+                }
+                else
+                    return (card1.rank >= card2.rank);
+            }
+            else
+            {
+                if (useTrumps && (card2.suit == Card.trump))
+                    return false;
+                else
+                    return true;
+            }
+        }
+        public static bool operator <=(Card card1, Card card2) => !(card1 > card2);
     }
 }
