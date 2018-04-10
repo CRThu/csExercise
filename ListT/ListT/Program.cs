487ec707812082c6f274e7eeff88134d5b57e20c
CRThu <1723001388@qq.com>
2018-04-10 12:15:47 +0800

    update ListT

--- ListT/ListT/Program.cs
+++ ListT/ListT/Program.cs
@@ -13,10 +13,25 @@ namespace ListT
             List<Animal> animalCollection = new List<Animal>();
             animalCollection.Add(new Cow("Rual"));
             animalCollection.Add(new Chicken("Donna"));
-            foreach(Animal myAnimal in animalCollection)
-            {
+            foreach (Animal myAnimal in animalCollection)
                 myAnimal.Feed();
-            }
+
+            Console.WriteLine();
+
+            // List<T> 的搜索和排序
+            Vectors route = new Vectors();
+            route.Add(new Vector(2.0, 90.0));
+            route.Add(new Vector(1.0, 180.0));
+            route.Add(new Vector(0.5, 45.0));
+            route.Add(new Vector(2.5, 315.0));
+            Console.WriteLine(route.Sum());
+            Comparison<Vector> sorter = new Comparison<Vector>(VectorDelegates.Compare);
+            route.Sort(sorter);
+            Console.WriteLine(route.Sum());
+            Predicate<Vector> searcher = new Predicate<Vector>(VectorDelegates.TopRightQuadrant);
+            Vectors topRightQuadrantRoute = new Vectors(route.FindAll(searcher));
+            Console.WriteLine(topRightQuadrantRoute.Sum());
+
             Console.ReadKey();
         }
     }
