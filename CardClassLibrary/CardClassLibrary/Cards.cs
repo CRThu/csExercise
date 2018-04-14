44d5e1cca15e793f2ad46598e9fd20957c54b01b
CRThu <1723001388@qq.com>
2018-04-14 09:03:26 +0800

    update CardClassLibrary

--- CardClassLibrary/CardClassLibrary/Cards.cs
+++ CardClassLibrary/CardClassLibrary/Cards.cs
@@ -7,29 +7,56 @@ using System.Threading.Tasks;
 
 namespace CardClassLibrary
 {
-    public class Cards : CollectionBase, ICloneable
+    //    public class Cards : CollectionBase, ICloneable
+    //    {
+    //        // 添加深度复制
+    //        public object Clone()
+    //        {
+    //            Cards newCards = new Cards();
+    //            foreach (Card sourceCard in List)
+    //                newCards.Add((Card)sourceCard.Clone());
+    //            return newCards;
+    //        }
+    //        // IList
+    //        public void Add(Card newCard)
+    //        {
+    //            List.Add(newCard);
+    //        }
+    //        public void Remove(Card oldCard)
+    //        {
+    //            List.Remove(oldCard);
+    //        }
+    //        public Card this[int cardIndex]
+    //        {
+    //            get { return (Card)List[cardIndex]; }
+    //            set { List[cardIndex] = value; }
+    //        }
+    //        ///<summary>
+    //        /// 复制实例至另一个实例的方法
+    //        /// 在Deck.Shuffle()使用
+    //        ///</summary>
+    //        public void CopyTo(Cards targetCards)
+    //        {
+    //            for (int index = 0; index < this.Count; index++)
+    //                targetCards[index] = this[index];
+    //        }
+    //        /// <summary>
+    //        /// 检查是否包含特定卡片
+    //        /// ArrayList中Contains()方法, 可以访问InnerList属性
+    //        /// </summary>
+    //        /// 
+    //        public bool Contains(Card card) => InnerList.Contains(card);
+    //    }
+
+    public class Cards : List<Card>, ICloneable
     {
         // 添加深度复制
         public object Clone()
         {
             Cards newCards = new Cards();
-            foreach (Card sourceCard in List)
+            foreach (Card sourceCard in this)
                 newCards.Add((Card)sourceCard.Clone());
             return newCards;
-        }
-        // IList
-        public void Add(Card newCard)
-        {
-            List.Add(newCard);
-        }
-        public void Remove(Card oldCard)
-        {
-            List.Remove(oldCard);
-        }
-        public Card this[int cardIndex]
-        {
-            get { return (Card)List[cardIndex]; }
-            set { List[cardIndex] = value; }
         }
         ///<summary>
         /// 复制实例至另一个实例的方法
@@ -40,11 +67,5 @@ namespace CardClassLibrary
             for (int index = 0; index < this.Count; index++)
                 targetCards[index] = this[index];
         }
-        /// <summary>
-        /// 检查是否包含特定卡片
-        /// ArrayList中Contains()方法, 可以访问InnerList属性
-        /// </summary>
-        /// 
-        public bool Contains(Card card) => InnerList.Contains(card);
     }
 }
