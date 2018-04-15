5d43a6ebaf9a3de6d59c1a945069ec113ea5d17c
CRThu <1723001388@qq.com>
2018-04-15 22:46:57 +0800

    update ClassT

---
+++ ClassT/ClassT/MyGenericClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassT
+{
+    class MyGenericClass<T1,T2,T3>
+    {
+        private T1 innerT1Object;
+        public MyGenericClass(T1 item)
+        {
+            innerT1Object = item;
+        }
+        public T1 InnerT1Object
+        {
+            get { return innerT1Object; }
+        }
+    }
+}
